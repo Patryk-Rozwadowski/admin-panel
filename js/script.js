0038b24f597c77d75b070503ffe04eab0fac52e3
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-03-17 00:31:20 +0100

    Add chart and styles

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function closeModal() {
 // CLICK OUTSIDE CLOSE
 overlay.addEventListener('click', function(e) {
   if(e.target === this) {
-    overlay.remove();
+    overlay.style.display = 'none'; // nie chce zadziałać overlay.classList.remove('show')
   }
 })
 // ESC QUIT
@@ -34,6 +34,43 @@ document.addEventListener('keyup', function(e) {
   }
 
 })
+
+// WYKRES
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88, 45],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 10, 47, 11, 50, 44, 63, 76, 23 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48,97 ],
+            // 7
+            hidden: true,
+        }]
+    },
+});
 /*
 
 document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
